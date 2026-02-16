43915e32fbfca87511322a8785ad2c9c65df2a89
Talha Ibn Hafiz <talhaibnhafiz1@gmail.com>
2026-02-16 09:47:09 +0600

    Change email link to use mailto protocol

--- src/config/index.ts
+++ src/config/index.ts
@@ -15,7 +15,7 @@ export const SITE_CONFIG: SiteConfig = {
   socialLinks: [
     { text: "LinkedIn", href: "https://linkedin.com/in/talhaibnhafiz" },
     { text: "Github", href: "https://github.com/talhaibnhafiz" },
-    { text: "Email", href: "talhaibnhafiz2007@gmail.com" },
+    { text: "Email", href: "mailto:talhaibnhafiz2007@gmail.com" },
   ],
   socialImage: "/7d1d28a3-b269-4562-9f32-356c52708d8c.jpg",
   canonicalURL: "https://astro-zen.vercel.app",
